47f602203a235b82d1a592d9421e7e74e1d19add
alexgeeshau <gichaumburu@gmail.com>
2022-09-02 13:36:22 +0300

    completed css

---
+++ components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1 className='text-cursive'>Alex Gichau</h1>
+        <h5 className='text-light'>Pro Graphics Designer, 2D Animator</h5>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ components/testimonials/Testimonails.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './testimonials.css'
+
+const Testimonails = () => {
+  return (
+    <div>Testimonails</div>
+  )
+}
+
+export default Testimonails
