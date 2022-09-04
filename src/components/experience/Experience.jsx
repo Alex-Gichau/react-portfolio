25dec7b978526afecd805eeefe67f1d39fa0ea7a
alexgeeshau <gichaumburu@gmail.com>
2022-09-05 02:03:06 +0300

    new react-portfolio

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,12 +1,98 @@
-import React from 'react'
-import './experience.css'
+import React from 'react';
+import './experience.css';
+import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Experience = () => {
   return (
-    <section id="experience">
-      
-    </section>
-  )
-}
+    <section id='experience'>
+      <h5>What Skills I have gained</h5>
+      <h2>My Experience</h2>
 
-export default Experience
+      <div className='container experience__container'>
+        <div className='experience__frontend'>
+          <h3>Frontend/Programming Skills</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons' />
+              <div>
+                <h4>HTML,CSS, XML</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>Java/Kotlin</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>Javascript/React JS</h4>
+                <small className='text-light'>Beginner</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>CISCO Networking</h4>
+                <small className='text-light'>Advanced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>UI/UX Design</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRONTEND DEVELOPMENT */}
+        <div className='experience_media'>
+          <h3>Graphics/Media Experience</h3>
+          <div className='experience__content'>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>Adobe Creative Cloud Suite</h4>
+                <small className='text-light'>Advanced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>Image/Video Editting</h4>
+                <small className='text-light'>Advanced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>Streaming/Production</h4>
+                <small className='text-light'>Advanced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>SEO/Keyword Optimization</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icons'/>
+              <div>
+                <h4>2D Animations</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,87 @@
-import React from 'react'
-import './portfolio.css'
+import React from 'react';
+import './portfolio.css';
+import IMG1 from '../../assets/alex2.png';
+
+const data = [
+  {
+    id:1,
+    image:IMG1,
+    title:'Crypto Currency Dashboard',
+    github:'https://github.com/Alex-Gichau',
+    demo:'https://github.com/Alex-Gichau'
+  },
+  {
+    id:2,
+    image:IMG1,
+    title:'Crypto Currency Dashboard',
+    github:'https://github.com/Alex-Gichau',
+    demo:'https://github.com/Alex-Gichau'
+  },
+  {
+    id:3,
+    image:IMG1,
+    title:'Crypto Currency Dashboard',
+    github:'https://github.com/Alex-Gichau',
+    demo:'https://github.com/Alex-Gichau'
+  },
+  {
+    id:4,
+    image:IMG1,
+    title:'Crypto Currency Dashboard',
+    github:'https://github.com/Alex-Gichau',
+    demo:'https://github.com/Alex-Gichau'
+  },
+  {
+    id:5,
+    image:IMG1,
+    title:'Crypto Currency Dashboard',
+    github:'https://github.com/Alex-Gichau',
+    demo:'https://github.com/Alex-Gichau'
+  },
+  {
+    id:6,
+    image:IMG1,
+    title:Crypto,
+    github:'https://github.com/Alex-Gichau/react-portfolio',
+    demo:'https://github.com/Alex-Gichau'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">
-      
-    </section>
-  )
-}
+    <section id='portfolio'>
+      <h5>My Recents Works</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className='container portfolio__container'>
+        {
+          data.map(({id,image,title,github,demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <div className='portfolio__item-cta'>
+            <a href={github} className='btn'>
+              Github
+            </a>
+            <a
+              href={demo}
+              className='btn btn-primary'
+              target='_blank'
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+            )
+          }) 
+        }
+
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
