4e1fa2a53d101a311a45af0086222e42a7cba818
Alex Gichau <geeshau@kali>
2023-09-01 20:01:56 +0300

    cv update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href="https://drive.google.com/file/d/1yNZYIQFIeyiCe9sk4VCtpVz9r46Mt7Nk/view?usp=sharing" target="blank" className='btn btn-primary'>View CV</a>
+        <a href="https://docs.google.com/document/d/1zgbe-x4HFn_y-w6v5zjt_Fn8S_tVwqxy6nLwbmqJhas/edit?usp=sharing" target="blank" className='btn btn-primary'>View CV</a>
         <a href="mailto:gichaumburu@gmail.com" target="blank" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
