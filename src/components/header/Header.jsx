aa10bbac90125fe61472c4839551cf15b8f23210
Alex Gichau <gichaumburu@gmail.com>
2024-02-17 23:53:19 +0300

    changed to freelance SWE

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1 className='my-name'>Alex Gichau</h1>
-        <h4 className='text-light'>Pro Graphics Designer, Animator</h4>
+        <h1 className='my-name'>Alex M. Gichau</h1>
+        <h4 className='text-light'>Freelance Software Developer</h4>
         <CTA/>
         <HeaderSocials/>
 
